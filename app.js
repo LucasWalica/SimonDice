9b3a637830b90a4609deef66a5abe63c91896b70
LucasWalica <lucasw58@educastur.es>
2024-02-08 19:30:30 +0100

    test

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
+let eleccionesSimon = [];
+let eleccionesJugador = [];
 
+// se añade al array
+function seleccionar(data){
+    caballero = '<div class="charcont" id="cab"><div id="cabSalto"></div></div>';
+    demon = '<div class="charcont" id="demon"><div id="demSal"></div></div>'
+    lobo = '<div class="charcont" id="lobo"><div id="lobosalto"></div></div>';
+    samurai = '<div class="charcont" id="samurai"><div id="samcorrer"></div></div>';
+    // testing
+    eleccionesJugador.push(data);
+    return data;
+}
 
 document.addEventListener('DOMContentLoaded', function() {
 
     // se necesitará crear constantes de las cartas 
-    const caballero = '<div class="charcont" id="cab"><div id="cabSalto"></div></div>';
-    const demon = '<div class="charcont" id="demon"><div id="demSal"></div></div>'
-    const lobo = '<div class="charcont" id="lobo"><div id="lobosalto"></div></div>';
-    const samurai = '<div class="charcont" id="samurai"><div id="samcorrer"></div></div>';
-
-    let opciones = [caballero, demon, lobo, samurai];
-    let eleccionesSimon = [];
-    let eleccionesJugador = [];
-
-    // Rondas jugadas y ganadas
-    let rondas = 0;
-    let contadorRondas = document.querySelector('#roundCounter');
-    contadorRondas.innerHTML = rondas;
-    // Contador de peronajes restantes
-    let contadorRestantes = document.querySelector('#countRestantes');
-    contadorRestantes.innerHTML = rondas; 
-
-    // seleccion e insercion de datos en html
     function simonRandom(){
+
+        const caballero = '<div class="charcont" id="cab"><div id="cabSalto"></div></div>';
+        const demon = '<div class="charcont" id="demon"><div id="demSal"></div></div>'
+        const lobo = '<div class="charcont" id="lobo"><div id="lobosalto"></div></div>';
+        const samurai = '<div class="charcont" id="samurai"><div id="samcorrer"></div></div>';
+        
+        let opciones = [caballero, demon, lobo, samurai];
+        // seleccion e insercion de datos en html + deicision simon
         let espacio = document.querySelector('#simonInsertar');
         let decision = opciones[(Math.floor(Math.random() * 4))];
         espacio.innerHTML = decision;
-        return decision;
+        eleccionesSimon.push(decision);
     }
     
-    let vidaJugador = true;
-    // mostrar carta
-    simonRandom();
-    eleccionesSimon.push(simonRandom());
-    console.log(eleccionesSimon[0]);
+
+    // boton inicio
+    let iniciarButt = document.querySelector('#butt');
+    iniciarButt.addEventListener('click', function(){
+        simonRandom();
+        rondas=1;
+        let contadorRondas = document.querySelector('#roundCounter');
+        contadorRondas.innerHTML = rondas;
+    });
+
+
+
+    // listeners de jugador
+    let botonesJugador = document.querySelectorAll('.charcont2');
+    botonesJugador.forEach(boton => {
+        boton.addEventListener('click', function() {
+            if(eleccionesJugador.join('')===eleccionesSimon.join('')){
+                alert("meme")
+            }
+            else {alert("No meme")}
+            console.log("Jugador: "+eleccionesJugador.join(''));
+            console.log("Bot:     "+eleccionesSimon.join(''));
+        });
+    });
+
 
 });
-// si acierta volver a repetir ciclo
